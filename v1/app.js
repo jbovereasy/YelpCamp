8abff1e031ea96d34b524de49aaa5813e79b998f
Baltazar <johnpatrick.baltazar.935@my.csun.edu>
2018-10-03 13:37:34 -0700

    changed bootstrap

--- v1/app.js
+++ v1/app.js
@@ -5,12 +5,6 @@ var bodyParser = require("body-parser");
 var campgrounds = [
     {name: "Yosemite", image: "https://www.mercurynews.com/wp-content/uploads/2018/05/sjm-camping-06xx-06.jpg?w=620"},
     {name: "Joshua Tree", image: "http://www.visitcalifornia.com/sites/default/files/styles/welcome_image/public/VCW_D_Jtree_heroDE_JoshuaTrees_1280x642_downsized.jpg"},
-    {name: "Sequoia", image: "https://thumbnails.trvl-media.com/NvOaB1B8uRwubQMQk86ZwtoSSsg=/768x432/images.trvl-media.com/media/content/shared/images/travelguides/destination/6084619/Sequoia-National-Park-203745.jpg"},
-    {name: "Yosemite", image: "https://www.mercurynews.com/wp-content/uploads/2018/05/sjm-camping-06xx-06.jpg?w=620"},
-    {name: "Joshua Tree", image: "http://www.visitcalifornia.com/sites/default/files/styles/welcome_image/public/VCW_D_Jtree_heroDE_JoshuaTrees_1280x642_downsized.jpg"},
-    {name: "Sequoia", image: "https://thumbnails.trvl-media.com/NvOaB1B8uRwubQMQk86ZwtoSSsg=/768x432/images.trvl-media.com/media/content/shared/images/travelguides/destination/6084619/Sequoia-National-Park-203745.jpg"},
-    {name: "Yosemite", image: "https://www.mercurynews.com/wp-content/uploads/2018/05/sjm-camping-06xx-06.jpg?w=620"},
-    {name: "Joshua Tree", image: "http://www.visitcalifornia.com/sites/default/files/styles/welcome_image/public/VCW_D_Jtree_heroDE_JoshuaTrees_1280x642_downsized.jpg"},
     {name: "Sequoia", image: "https://thumbnails.trvl-media.com/NvOaB1B8uRwubQMQk86ZwtoSSsg=/768x432/images.trvl-media.com/media/content/shared/images/travelguides/destination/6084619/Sequoia-National-Park-203745.jpg"}
 ]
 
