8127a572d187a8798765787e232c5e003a226e39
John P Baltazar <john.baltazar03@gmail.com>
2018-11-01 16:17:21 -0700

    mongo

--- RESTfulApp/app.js
+++ RESTfulApp/app.js
@@ -4,7 +4,10 @@ var express = require("express"),
   mongoose = require("mongoose");
 
 // APP CONFIG
-mongoose.connect("mongodb://localhost/restful_blog_app");
+mongoose.connect(
+  "mongodb://localhost:27017/restful_blog_app",
+  { useNewUrlParser: true }
+);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,13 +20,20 @@ var blogSchema = new mongoose.Schema({
   created: { type: Date, default: Date.now }
 });
 var Blog = mongoose.model("Blog", blogSchema);
-
+``;
 // RESTful routes
 app.get("/", function(req, res) {
   res.redirect("/blogs");
 });
 
 app.get("/blogs", function(req, res) {
+  Blog.find({}, function(err, blogs) {
+    if (err) {
+      console.log("Error!");
+    } else {
+      res.render("index", { blogs: blogs });
+    }
+  });
   res.render("index");
 });
 
